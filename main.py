c8b7ea3c610b6899fe32fd470e163c23a2accb2b
Harry Sharma <harrysharma1066@gmail.com>
2024-12-03 14:00:37 +0000

    feat: add bibtex type

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+import uuid
+
+
+parser = argparse.ArgumentParser(prog="Bibtext Generator", description="Generate Bibtex from URLs")
+parser.add_argument("url")
+parser.add_argument("-k", "--key",default=uuid.uuid4())
+parser.add_argument("-a", "--author")
+parser.add_argument("-r", "--release")
+
+args = parser.parse_args()
+
+
+text = "@online"
+text += "{"
+text += "{},\n".format(args.key)
+text += "author = {"
+
+author_name = ", ".join(args.author.split(' ')[::-1])
+text += "{}".format(author_name)
+text += "},\n"
+text += "title = {}"
+
+
+print(text)
