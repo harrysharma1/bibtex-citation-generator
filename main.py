2e2fea92d01f2aa833c45ca0246d8fc2bac54215
Harry Sharma <harrysharma1066@gmail.com>
2024-12-04 12:36:44 +0000

    feat: add arxiv articles

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import argparse
+from ssl import SSLEOFError, SSLError
 import uuid
 from bs4 import BeautifulSoup
 import requests
 from datetime import date
 import subprocess
+import arxiv
+import string
+arxiv_client = arxiv.Client()
 
 def extract_title(url):
     html = requests.get(url)
@@ -19,6 +23,31 @@ def get_current_year():
     today = date.today()
     return today.strftime("%/Y")
 
+def arxiv_to_bibtex(url):
+    full_url = url
+    url = url.strip()
+    url = url.replace("arxiv:", "")
+    url = url.replace("https://", "")
+    url = url.replace("www.", "")
+    url = url.replace("arxiv.org/abs/", "")
+    url = url.split('v')[0]
+    xid = url.strip()
+    print(xid)
+    try:
+        search_by_id = arxiv.Search(id_list=[xid])
+        first_result = next(arxiv_client.results(search_by_id))
+        print(first_result.title)
+    except arxiv.HTTPError as e:
+        print(f"HTTPError: Could not retrieve the paper. Status code: {e.status}")
+    except StopIteration:
+        print("Error: No results found for the given arXiv ID.")
+    except SSLError as e:
+        print(f"SSLError has occurred: {e}")
+    except SSLEOFError as e:
+        print(f"SSLEOFError has occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 parser = argparse.ArgumentParser(prog="Bibtext Generator", description="Generate Bibtex from URLs")
 parser.add_argument("url")
 parser.add_argument("-t", "--type", default="misc")
@@ -30,30 +59,33 @@ parser.add_argument("-l", "--last-accessed", default=get_current_date())
 args = parser.parse_args()
 
 def online_bibtex():
-    print("Starting generation...")
-    text = "@{}".args.type
-    text += "{"
-    text += " {},\n".format(args.key)
-    text += "   author = {"
-    author_name = ", ".join(args.author.split(' ')[::-1])
-    text += "{}".format(author_name)
-    text += "},\n"
-    text += "   title = {"
-    text += "{}".format(extract_title(args.url))
-    text += "},\n"
-    text += "   url = {"
-    text +="{}".format(args.url)
-    text +="},\n"
-    text +="   addendum = {last accessed: "
-    text += "{}".format(args.last_accessed)
-    text += "},\n"
-    text += "   year = {"
-    text += "{}".format(args.release)
-    text += "}\n}"
-    subprocess.run("pbcopy", text=True, input=text)
-    print("Finished generating:\n{}".format(text))
-    print("Copied to clipboard")
-    
+    if "https://arxiv.org" in args.url:
+       arxiv_to_bibtex(args.url) 
+    else:
+        print("Starting generation...")
+        text = "@{}".args.type
+        text += "{"
+        text += " {},\n".format(args.key)
+        text += "   author = {"
+        author_name = ", ".join(args.author.split(' ')[::-1])
+        text += "{}".format(author_name)
+        text += "},\n"
+        text += "   title = {"
+        text += "{}".format(extract_title(args.url))
+        text += "},\n"
+        text += "   url = {"
+        text +="{}".format(args.url)
+        text +="},\n"
+        text +="   addendum = {last accessed: "
+        text += "{}".format(args.last_accessed)
+        text += "},\n"
+        text += "   year = {"
+        text += "{}".format(args.release)
+        text += "}\n}"
+        subprocess.run("pbcopy", text=True, input=text)
+        print("Finished generating:\n{}".format(text))
+        print("Copied to clipboard")
+        
 def misc_bibtex():
     print("Starting generation...")
     text = "@{}".format(args.type)
