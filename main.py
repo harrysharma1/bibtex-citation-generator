cce936da2073da094bcb12c8ff119cab78c6d6f2
Harry Sharma <harrysharma1066@gmail.com>
2024-12-04 22:44:27 +0000

    feat: add help text for flags and args

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import uuid
 
 utility = BibtexUtility()
 
-parser = argparse.ArgumentParser(prog="Bibtext Generator", description="Generate Bibtex from URLs")
-parser.add_argument("url")
-parser.add_argument("-t", "--type", default="misc")
-parser.add_argument("-k", "--key", default=uuid.uuid4())
-parser.add_argument("-a", "--author")
-parser.add_argument("-r", "--release", default=utility.get_current_year())
-parser.add_argument("-l", "--last-accessed", default=utility.get_current_date())
+parser = argparse.ArgumentParser(prog="Bibtext Generator", description="Generate Bibtex from URLs.")
+parser.add_argument("url", help="URL for document you would like to cite.")
+parser.add_argument("-t", "--type", default="misc", help="The type of document you believe it is. It will default to @misc if not provided.")
+parser.add_argument("-k", "--key", default=str(uuid.uuid4()), help="Citation key for bibtex. If not provided, it will default to a UUID string.")
+parser.add_argument("-a", "--author", default="<Alter Please>", help="Author name(s). If not provided, will default to <Alter Please>")
+parser.add_argument("-r", "--release", default=utility.get_current_year(), help="Release date of the article. If not provided, will default to current year.")
+parser.add_argument("-l", "--last-accessed", default=utility.get_current_date(), help="Last accessed article date. If not provided, will default to current date.")
 
 args = parser.parse_args()
 
