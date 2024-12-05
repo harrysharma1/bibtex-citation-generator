a5aa61f71d927d01542e35f78780b09781d00f8d
Harry Sharma <harrysharma1066@gmail.com>
2024-12-05 09:32:44 +0000

    feat: add arxiv capabilities for any type but for now misc and online

--- src/arxiv_search.py
+++ src/arxiv_search.py
@@ -1,12 +1,12 @@
 from ssl import SSLEOFError, SSLError
-import uuid
+import subprocess
 import arxiv
 
 class ArxivSearch():
     def __init__(self) -> None:
         self.arxiv_client = arxiv.Client()
     
-    def arxiv_to_bibtex(self, url, key):
+    def arxiv_to_bibtex(self, url, type, key):
         full_url = url
         url = url.strip()
         url = url.replace("arxiv:", "")
@@ -20,13 +20,38 @@ class ArxivSearch():
             first_result = next(self.arxiv_client.results(search_by_id))
             print("Starting generation...")
             text = ""
-            text += "@misc{"
-            text += " {},\n".format(key)
+            text += "@{}".format(type) 
+            text += "{"
+            text += "{},\n".format(key)
             text += "   title = {"
             text += "{}".format(first_result.title)
             text += "},\n"
             text += "   author = {"
+            for i in range(len(first_result.authors)):
+                if i < len(first_result.authors)-1:
+                    text += "{} and ".format(first_result.authors[i])
+                else:
+                    text += "{}".format(first_result.authors[i])
+            text += "},\n"
+            text += "   year = {"
+            text += "{}".format(first_result.published.strftime("%Y"))
+            text += "},\n"
+            text += "   eprint = {"
+            text += "{}".format(first_result.get_short_id())
+            text += "},\n"
+            text += "   archivePrefix = {arXiv"
+            text += "},\n"
+            text += "   primaryClass = {"
+            text += "{}".format(first_result.primary_category)
+            text += "},\n"
+            text += "   url = {"
+            text += "{}".format(first_result.entry_id)
+            text += "},\n"
+            text += "}"
+            print("Finished generating:")
             print(text)
+            subprocess.run("pbcopy", text=True, input=text)
+            print("Copied to clipboard")
         except arxiv.HTTPError as e:
             print(f"HTTPError: Could not retrieve the paper. Status code: {e.status} for url: {full_url}")
         except StopIteration:
--- src/bibtex.py
+++ src/bibtex.py
@@ -10,7 +10,7 @@ class Bibtex():
         self.citation_key = uuid.uuid4()
         self.arxiv_search = ArxivSearch()
         self.utility_function = BibtexUtility()
-        self.subprocess = subprocess
+       
         
     @abstractmethod
     def get_bibtex(self, url):
@@ -23,7 +23,7 @@ class OnlineBibtex(Bibtex):
     
     def get_bibtex(self, url, type, key, author, last_accessed, release):
         if "https://arxiv.org" in url:
-            self.arxiv_search.arxiv_to_bibtex(url, key)
+            self.arxiv_search.arxiv_to_bibtex(url, type, key)
         else:
             print("Starting generation...")
             text = "@{}".format(type)
@@ -45,7 +45,7 @@ class OnlineBibtex(Bibtex):
             text += "   year = {"
             text += "{}".format(release)
             text += "}\n}"
-            self.subprocess.run("pbcopy", text=True, input=text)
+            subprocess.run("pbcopy", text=True, input=text)
             print("Finished generating:\n{}".format(text))
             print("Copied to clipboard")
 
@@ -54,26 +54,29 @@ class MiscBibtext(Bibtex):
         super().__init__()
         
     def get_bibtex(self, url, type, key, author, last_accessed, release):
-        print("Starting generation...")
-        text = "@{}".format(type)
-        text += "{"
-        text += "{},\n".format(key)
-        text += "   author = {"
-        author_name = ", ".join(author.split(' ')[::-1])
-        text += "{}".format(author_name)
-        text += "},\n"
-        text += "   title = {"
-        text += "{}".format(self.utility_function.extract_title(url))
-        text += "},\n"
-        text += "   url = {"
-        text +="{}".format(url)
-        text +="},\n"
-        text +="   addendum = {last accessed: "
-        text += "{}".format(last_accessed)
-        text += "},\n"
-        text += "   year = {"
-        text += "{}".format(release)
-        text += "}\n}"
-        self.subprocess.run("pbcopy", text=True, input=text)
-        print("Finished generating:\n{}".format(text))
-        print("Copied to clipboard")
+        if "https://arxiv.org" in url:
+            self.arxiv_search.arxiv_to_bibtex(url, type, key)
+        else:      
+            print("Starting generation...")
+            text = "@{}".format(type)
+            text += "{"
+            text += "{},\n".format(key)
+            text += "   author = {"
+            author_name = ", ".join(author.split(' ')[::-1])
+            text += "{}".format(author_name)
+            text += "},\n"
+            text += "   title = {"
+            text += "{}".format(self.utility_function.extract_title(url))
+            text += "},\n"
+            text += "   url = {"
+            text +="{}".format(url)
+            text +="},\n"
+            text +="   addendum = {last accessed: "
+            text += "{}".format(last_accessed)
+            text += "},\n"
+            text += "   year = {"
+            text += "{}".format(release)
+            text += "}\n}"
+            subprocess.run("pbcopy", text=True, input=text)
+            print("Finished generating:\n{}".format(text))
+            print("Copied to clipboard")
