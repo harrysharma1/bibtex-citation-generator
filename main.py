f4f4e1f250cf34b94c4b31ef4a14a8af12b5dd25
Harry Sharma <harrysharma1066@gmail.com>
2024-12-05 12:57:01 +0000

    update: refactor so that type is the command with url subcommands rather than an option type

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ import uuid
 utility = BibtexUtility()
 
 parser = argparse.ArgumentParser(prog="Bibtext Generator", description="Generate Bibtex from URLs.")
-parser.add_argument("url", help="URL for document you would like to cite.")
-parser.add_argument("-t", "--type", default="misc", help="The type of document you believe it is. It will default to @misc if not provided.")
-parser.add_argument("-k", "--key", default=str(uuid.uuid4()), help="Citation key for bibtex. If not provided, it will default to a UUID string.")
-parser.add_argument("-a", "--author", default="<Alter Please>", help="Author name(s). If not provided, will default to <Alter Please>")
-parser.add_argument("-r", "--release", default=utility.get_current_year(), help="Release date of the article. If not provided, will default to current year.")
-parser.add_argument("-l", "--last-accessed", default=utility.get_current_date(), help="Last accessed article date. If not provided, will default to current date.")
+subparsers = parser.add_subparsers(dest="command", help="Type of bibtex entry")
+
+# @online
+online_parser = subparsers.add_parser("online", help="Generate bibtex for online document.")
+online_parser.add_argument("url", help="URL for document you would like to cite.")
+online_parser.add_argument("-k", "--key", default=str(uuid.uuid4()), help="Citation key for bibtex. If not provided, it will default to a UUID string.")
+online_parser.add_argument("-a", "--author", default="<Alter Please>", help="Author name(s). If not provided, will default to <Alter Please>")
+online_parser.add_argument("-r", "--release", default=utility.get_current_year(), help="Release date of the article. If not provided, will default to current year.")
+online_parser.add_argument("-l", "--last-accessed", default=utility.get_current_date(), help="Last accessed article date. If not provided, will default to current date.")
+
+# @misc
+misc_parser = subparsers.add_parser("misc", help="Generate bibtex for miscellaneous document.")
+misc_parser.add_argument("url", help="URL for document you would like to cite.")
+misc_parser.add_argument("-k", "--key", default=str(uuid.uuid4()), help="Citation key for bibtex. If not provided, it will default to a UUID string.")
+misc_parser.add_argument("-a", "--author", default="<Alter Please>", help="Author name(s). If not provided, will default to <Alter Please>")
+misc_parser.add_argument("-r", "--release", default=utility.get_current_year(), help="Release date of the article. If not provided, will default to current year.")
+misc_parser.add_argument("-l", "--last-accessed", default=utility.get_current_date(), help="Last accessed article date. If not provided, will default to current date.")
 
 args = parser.parse_args()
 
-match args.type:
+match args.command:
     case "online":
-        OnlineBibtex().get_bibtex(args.url, args.type, args.key, args.author, args.last_accessed, args.release)
+        OnlineBibtex().get_bibtex(args.url, args.command, args.key, args.author, args.last_accessed, args.release)
     case "misc":
-        MiscBibtext().get_bibtex(args.url, args.type, args.key, args.author, args.last_accessed, args.release)
+        MiscBibtext().get_bibtex(args.url, args.command, args.key, args.author, args.last_accessed, args.release)
     case _:
         "Error: No type provided"
